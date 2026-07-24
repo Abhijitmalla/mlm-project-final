eed2c08903a8f818446ff995c5fba13bf686706b
Abhijitmalla <abhijitmalla06@gmail.com>
2026-07-24 11:11:11 +0530

    done

--- public/api-config.js
+++ public/api-config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    // API_BASE_URL: 'https://vkservicesenterprise.in/api',
-        API_BASE_URL: 'http://localhost:5000/api',
+    API_BASE_URL: 'https://vkservicesenterprise.in/api',
+        // API_BASE_URL: 'http://localhost:5000/api',
 
 };
